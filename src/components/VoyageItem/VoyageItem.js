98853f1388e646eaebd402317e1b76c37a35833e
unknown <anasmebarki1996@outlook.fr>
2021-04-22 20:58:18 +0100

    second commit

--- src/components/VoyageItem/VoyageItem.js
+++ src/components/VoyageItem/VoyageItem.js
@@ -2,11 +2,12 @@ import React from "react";
 import "./style.css";
 
 import { BsArrowRight } from "react-icons/bs";
+import VoyageDetails from "../ VoyageDetails/VoyageDetails";
 
 const VoyageItem = () => {
   return (
-    <div className="container">
-      <div className="row">
+    <div className="container-fluid">
+      <div className="row d-flex justify-content-center">
         <div className="col-12 col-md-5 table">
           <div className="row column_item table-header">
             <div className="col-4 col-md-9 item">icon 18 mars 2020 </div>
@@ -45,31 +46,7 @@ const VoyageItem = () => {
           </div>
         </div>
         <div className="col-12 col-md-5 table">
-          <div className="row">
-            <div className="col-6">
-              <div>outBound</div>
-              <div>$56.61 </div>
-            </div>
-            <div className="col-6">1 Adult</div>
-          </div>
-          <div>selected journey</div>
-          <div className="box-container">
-            <div className="row">
-              <div className="col-6">05:52 </div>
-              <div className="col-6">Paris Gare de Lyon </div>
-            </div>
-            <div className="row">
-              <div className="col-6">5min </div>
-              <div className="col-6"></div>
-            </div>
-            <div className="row">
-              <div className="col-6">05:52 </div>
-              <div className="col-6">Paris Gare de Lyon </div>
-            </div>
-          </div>
-          <div>
-            <button>reserver</button>{" "}
-          </div>
+          <VoyageDetails />
         </div>
       </div>
     </div>
